4d06e4bffd40857a19963ae23ac32d41b24e62cb
squirrelcolab <mrugankdevendradake@students.iisertirupati.ac.in>
2020-09-11 18:39:36 +0000

    created vectors for rnd

--- test_calc_features.py
+++ test_calc_features.py
@@ -17,22 +17,26 @@ spec_dir = os.path.join(Project_path, 'squirrel_note_embeddings', species)
 if not os.path.exists(spec_dir):
   os.mkdir(spec_dir)
 all_fs = os.listdir(audio_dir)
+print('Listing audio files')
 audio_fs_temp = [f for f in all_fs if '.wav' in f.lower() or '.mp3' in f.lower()]
-audio_fs = []
-for temp_file in audio_fs_temp:
-  temp_path = os.path.join(audio_dir, temp_file)
-  with contextlib.closing(wave.open(temp_path,'r')) as fofo:
-    frames = fofo.getnframes()
-    rate = fofo.getframerate()
-  duration = frames / float(rate)
-  #print(duration)
-  if duration > 0.98:
-    audio_fs.append(temp_file)
+print('2 check')
 
+#audio_fs = []
+#for temp_file in audio_fs_temp:
+#  temp_path = os.path.join(audio_dir, temp_file)
+#  with contextlib.closing(wave.open(temp_path,'r')) as fofo:
+#    frames = fofo.getnframes()
+#    rate = fofo.getframerate()
+#  duration = frames / float(rate)
+  #print(duration)
+#  if duration > 0.98:
+#    audio_fs.append(temp_file)
+audio_fs = audio_fs_temp
+print('3 check')
 # Setup the audioset analysis
 an = AudiosetAnalysis()
 an.setup()
-
+print('4 check')
 # Analyse each audio file in turn, and print the shape of the results
 for f in audio_fs:
     path = os.path.join(audio_dir, f)
