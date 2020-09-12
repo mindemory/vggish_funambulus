a9d56003e2710bced36d9614b6e51403da5b22b1
squirrelcolab <mrugankdevendradake@students.iisertirupati.ac.in>
2020-09-12 17:04:16 +0000

    random forest for rnd

--- analysis_libs_funambulus_with_noise.py
+++ analysis_libs_funambulus_with_noise.py
@@ -40,36 +40,348 @@ def multi_class_classification(X, y, k_fold = 5):
   '''
   X = np.asarray(X)
   y = np.asarray(y)
-  
-  #X_noise = []
-  #X_normal = []
-  #y_noise = []
-  #y_normal = []
-  #new_X_noise = []
-  #new_y_noise = []
-  #for i in range(y.shape[0]):
-  #  if y[i] == 'noise':
-  #    X_noise.append(X[i])
-  #    y_noise.append(y[i])
-  #  else:
-  #    X_normal.append(X[i])
-  #    y_normal.append(y[i])
-  
-  #data_pos = np.arange(len(y_noise))
-  #chosen_data_pos = np.random.choice(data_pos, 10000)
-  
-  #for pos in chosen_data_pos:
-  #  new_X_noise.append(X_noise[pos])
-  #  new_y_noise.append(y_noise[pos])
 
-  #X_normal = np.asarray(X_normal)
-  #new_X_noise = np.asarray(X_noise)
-  #y_normal = np.asarray(y_normal)
-  #new_y_noise = np.asarray(y_noise)
-  
-  #X = np.vstack((X_normal, new_X_noise))
-  #y = np.vstack((y_normal, new_y_noise))
+  # dividing X, y into train and test data
+  sss = StratifiedShuffleSplit(n_splits=k_fold, test_size=0.2, random_state=0)
 
+  # Do K fold cross validation
+  all_cms = []
+  all_accuracies = []
+  tp_array = []
+  fp_array = []
+  tn_array = []
+  fn_array = []
+  print('Doing {} fold cross validation predictions. Classes: {}'.format(k_fold,np.unique(y)))
+  for k, (train_index, test_index) in enumerate(sss.split(X, y)):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    #X_train_corrected = 
+    species = ['noise', 'ratufa', 'dusky']
+    train_res = {}
+    test_res = {}
+    for sp in species:
+      train_res[sp] = 0
+      test_res[sp] = 0
+
+    for i in y_train:
+      train_res[i] += 1
+    
+    for i in y_test:
+      test_res[i] += 1
+
+    print("Training set = {}".format(train_res))
+    print("Testing set = {}".format(test_res))
+    # training a classifier
+    clf = RandomForestClassifier(random_state=0, n_estimators=100)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+
+    # model accuracy for X_test
+    class_scores = f1_score(y_test,predictions,average=None)
+    print('{}/{} folds mean accuracy: {}'.format(k+1,k_fold,np.mean(class_scores)))
+    all_accuracies.append(class_scores)
+
+    cm_labels = np.unique(y)
+    k_cm = confusion_matrix(y_test, predictions, labels=cm_labels)
+    FP = k_cm.sum(axis=0) - np.diag(k_cm)  
+    FN = k_cm.sum(axis=1) - np.diag(k_cm)
+    TP = np.diag(k_cm)
+    TN = k_cm.sum().sum() - (FP + FN + TP)  
+    tp_array.append(TP)
+    fp_array.append(FP)
+    tn_array.append(TN)
+    fn_array.append(FN)    
+    all_cms.append(k_cm)
+
+  # Get averages across K fold cross validation
+  final_tp = np.mean(np.asarray(tp_array), axis = 0)
+  final_tn = np.mean(np.asarray(tn_array), axis = 0)
+  final_fp = np.mean(np.asarray(fp_array), axis = 0)
+  final_fn = np.mean(np.asarray(fn_array), axis = 0)
+  cm_values = [final_tp, final_tn, final_fp, final_fn]
+  accuracies = np.mean(np.asarray(all_accuracies),axis=0)
+  average_accuracy = np.mean(accuracies)
+  print('Average accuracy = {}'.format(average_accuracy))
+
+  cm = np.mean(np.asarray(all_cms),axis=0)
+
+  return cm, cm_labels, average_accuracy, accuracies, cm_values
+
+def multi_class_classification_a(X, y, k_fold = 5):
+  '''
+  Do a multiclass classification task using a random forest classifier
+  Accuracy is measured using f1 score
+
+  Inputs:
+    X (ndarray): feature data
+    y (ndarray): labels associated with feature data
+    k_fold (int): number of cross-fold validation runs to use
+
+  Returns:
+    (All of the below are averaged from cross-fold validation results)
+    cm (ndarray): confusion matrix of results
+    cm_labels (ndarray): labels for the confusion matrix
+    average_accuracy (float): average accuracy across all classes
+    accuracies (ndarray): individual accuracies for each class
+  '''
+  X = np.asarray(X)
+  y = np.asarray(y)
+  X_noise = []
+  X_normal = []
+  y_noise = []
+  y_normal = []
+  new_X_noise = []
+  new_y_noise = []
+  for i in range(y.shape[0]):
+    if y[i] == 'noise':
+      X_noise.append(X[i])
+      y_noise.append(y[i])
+    else:
+      X_normal.append(X[i])
+      y_normal.append(y[i])
+  
+  data_pos = np.arange(len(y_noise))
+  chosen_data_pos = np.random.choice(data_pos, 15000)
+  
+  for pos in chosen_data_pos:
+    new_X_noise.append(X_noise[pos])
+    new_y_noise.append(y_noise[pos])
+
+  X_normal = np.asarray(X_normal)
+  new_X_noise = np.asarray(X_noise)
+  y_normal = np.asarray(y_normal)
+  new_y_noise = np.asarray(y_noise)
+  
+  X = np.vstack((X_normal, new_X_noise))
+  y = np.vstack((y_normal, new_y_noise))
+
+  # dividing X, y into train and test data
+  sss = StratifiedShuffleSplit(n_splits=k_fold, test_size=0.2, random_state=0)
+
+  # Do K fold cross validation
+  all_cms = []
+  all_accuracies = []
+  tp_array = []
+  fp_array = []
+  tn_array = []
+  fn_array = []
+  print('Doing {} fold cross validation predictions. Classes: {}'.format(k_fold,np.unique(y)))
+  for k, (train_index, test_index) in enumerate(sss.split(X, y)):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    #X_train_corrected = 
+    species = ['noise', 'ratufa', 'dusky']
+    train_res = {}
+    test_res = {}
+    for sp in species:
+      train_res[sp] = 0
+      test_res[sp] = 0
+
+    for i in y_train:
+      train_res[i] += 1
+    
+    for i in y_test:
+      test_res[i] += 1
+
+    print("Training set = {}".format(train_res))
+    print("Testing set = {}".format(test_res))
+    # training a classifier
+    clf = RandomForestClassifier(random_state=0, n_estimators=100)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+
+    # model accuracy for X_test
+    class_scores = f1_score(y_test,predictions,average=None)
+    print('{}/{} folds mean accuracy: {}'.format(k+1,k_fold,np.mean(class_scores)))
+    all_accuracies.append(class_scores)
+
+    cm_labels = np.unique(y)
+    k_cm = confusion_matrix(y_test, predictions, labels=cm_labels)
+    FP = k_cm.sum(axis=0) - np.diag(k_cm)  
+    FN = k_cm.sum(axis=1) - np.diag(k_cm)
+    TP = np.diag(k_cm)
+    TN = k_cm.sum().sum() - (FP + FN + TP)  
+    tp_array.append(TP)
+    fp_array.append(FP)
+    tn_array.append(TN)
+    fn_array.append(FN)    
+    all_cms.append(k_cm)
+
+  # Get averages across K fold cross validation
+  final_tp = np.mean(np.asarray(tp_array), axis = 0)
+  final_tn = np.mean(np.asarray(tn_array), axis = 0)
+  final_fp = np.mean(np.asarray(fp_array), axis = 0)
+  final_fn = np.mean(np.asarray(fn_array), axis = 0)
+  cm_values = [final_tp, final_tn, final_fp, final_fn]
+  accuracies = np.mean(np.asarray(all_accuracies),axis=0)
+  average_accuracy = np.mean(accuracies)
+  print('Average accuracy = {}'.format(average_accuracy))
+
+  cm = np.mean(np.asarray(all_cms),axis=0)
+
+  return cm, cm_labels, average_accuracy, accuracies, cm_values
+
+def multi_class_classification_b(X, y, k_fold = 5):
+  '''
+  Do a multiclass classification task using a random forest classifier
+  Accuracy is measured using f1 score
+
+  Inputs:
+    X (ndarray): feature data
+    y (ndarray): labels associated with feature data
+    k_fold (int): number of cross-fold validation runs to use
+
+  Returns:
+    (All of the below are averaged from cross-fold validation results)
+    cm (ndarray): confusion matrix of results
+    cm_labels (ndarray): labels for the confusion matrix
+    average_accuracy (float): average accuracy across all classes
+    accuracies (ndarray): individual accuracies for each class
+  '''
+  X = np.asarray(X)
+  y = np.asarray(y)
+  X_noise = []
+  X_normal = []
+  y_noise = []
+  y_normal = []
+  new_X_noise = []
+  new_y_noise = []
+  for i in range(y.shape[0]):
+    if y[i] == 'noise':
+      X_noise.append(X[i])
+      y_noise.append(y[i])
+    else:
+      X_normal.append(X[i])
+      y_normal.append(y[i])
+  
+  data_pos = np.arange(len(y_noise))
+  chosen_data_pos = np.random.choice(data_pos, 7000)
+  
+  for pos in chosen_data_pos:
+    new_X_noise.append(X_noise[pos])
+    new_y_noise.append(y_noise[pos])
+
+  X_normal = np.asarray(X_normal)
+  new_X_noise = np.asarray(X_noise)
+  y_normal = np.asarray(y_normal)
+  new_y_noise = np.asarray(y_noise)
+  
+  X = np.vstack((X_normal, new_X_noise))
+  y = np.vstack((y_normal, new_y_noise))
+
+  # dividing X, y into train and test data
+  sss = StratifiedShuffleSplit(n_splits=k_fold, test_size=0.2, random_state=0)
+
+  # Do K fold cross validation
+  all_cms = []
+  all_accuracies = []
+  tp_array = []
+  fp_array = []
+  tn_array = []
+  fn_array = []
+  print('Doing {} fold cross validation predictions. Classes: {}'.format(k_fold,np.unique(y)))
+  for k, (train_index, test_index) in enumerate(sss.split(X, y)):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    #X_train_corrected = 
+    species = ['noise', 'ratufa', 'dusky']
+    train_res = {}
+    test_res = {}
+    for sp in species:
+      train_res[sp] = 0
+      test_res[sp] = 0
+
+    for i in y_train:
+      train_res[i] += 1
+    
+    for i in y_test:
+      test_res[i] += 1
+
+    print("Training set = {}".format(train_res))
+    print("Testing set = {}".format(test_res))
+    # training a classifier
+    clf = RandomForestClassifier(random_state=0, n_estimators=100)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+
+    # model accuracy for X_test
+    class_scores = f1_score(y_test,predictions,average=None)
+    print('{}/{} folds mean accuracy: {}'.format(k+1,k_fold,np.mean(class_scores)))
+    all_accuracies.append(class_scores)
+
+    cm_labels = np.unique(y)
+    k_cm = confusion_matrix(y_test, predictions, labels=cm_labels)
+    FP = k_cm.sum(axis=0) - np.diag(k_cm)  
+    FN = k_cm.sum(axis=1) - np.diag(k_cm)
+    TP = np.diag(k_cm)
+    TN = k_cm.sum().sum() - (FP + FN + TP)  
+    tp_array.append(TP)
+    fp_array.append(FP)
+    tn_array.append(TN)
+    fn_array.append(FN)    
+    all_cms.append(k_cm)
+
+  # Get averages across K fold cross validation
+  final_tp = np.mean(np.asarray(tp_array), axis = 0)
+  final_tn = np.mean(np.asarray(tn_array), axis = 0)
+  final_fp = np.mean(np.asarray(fp_array), axis = 0)
+  final_fn = np.mean(np.asarray(fn_array), axis = 0)
+  cm_values = [final_tp, final_tn, final_fp, final_fn]
+  accuracies = np.mean(np.asarray(all_accuracies),axis=0)
+  average_accuracy = np.mean(accuracies)
+  print('Average accuracy = {}'.format(average_accuracy))
+
+  cm = np.mean(np.asarray(all_cms),axis=0)
+
+  return cm, cm_labels, average_accuracy, accuracies, cm_values
+
+def multi_class_classification_c(X, y, k_fold = 5):
+  '''
+  Do a multiclass classification task using a random forest classifier
+  Accuracy is measured using f1 score
+
+  Inputs:
+    X (ndarray): feature data
+    y (ndarray): labels associated with feature data
+    k_fold (int): number of cross-fold validation runs to use
+
+  Returns:
+    (All of the below are averaged from cross-fold validation results)
+    cm (ndarray): confusion matrix of results
+    cm_labels (ndarray): labels for the confusion matrix
+    average_accuracy (float): average accuracy across all classes
+    accuracies (ndarray): individual accuracies for each class
+  '''
+  X = np.asarray(X)
+  y = np.asarray(y)
+  X_noise = []
+  X_normal = []
+  y_noise = []
+  y_normal = []
+  new_X_noise = []
+  new_y_noise = []
+  for i in range(y.shape[0]):
+    if y[i] == 'noise':
+      X_noise.append(X[i])
+      y_noise.append(y[i])
+    else:
+      X_normal.append(X[i])
+      y_normal.append(y[i])
+  
+  data_pos = np.arange(len(y_noise))
+  chosen_data_pos = np.random.choice(data_pos, 3000)
+  
+  for pos in chosen_data_pos:
+    new_X_noise.append(X_noise[pos])
+    new_y_noise.append(y_noise[pos])
+
+  X_normal = np.asarray(X_normal)
+  new_X_noise = np.asarray(X_noise)
+  y_normal = np.asarray(y_normal)
+  new_y_noise = np.asarray(y_noise)
+  
+  X = np.vstack((X_normal, new_X_noise))
+  y = np.vstack((y_normal, new_y_noise))
 
   # dividing X, y into train and test data
   sss = StratifiedShuffleSplit(n_splits=k_fold, test_size=0.2, random_state=0)
--- multiclass_predictions_funambulus_with_noise.py
+++ multiclass_predictions_funambulus_with_noise.py
@@ -40,15 +40,13 @@ for f in feats:
     SQUIRRELS_LIST.append(toto)
   SQUIRRELS = np.array(SQUIRRELS_LIST)
   cm, cm_labs, average_acc, accuracies, cm_values = multi_class_classification(SQUIRRELS, species, k_fold=k_folds)
-  #cm1, cm_labs1, average_acc1, accuracies1, cm_values1 = multi_class_classification_a(SQUIRRELS, species, k_fold=k_folds)
-  #cm2, cm_labs2, average_acc2, accuracies2, cm_values2 = multi_class_classification_b(SQUIRRELS, species, k_fold=k_folds)
   
   plot_multi_class_recalls(accuracies, cm_labs, average_acc, cm_values, 'species', f)
   ax.set_title('Species classification')
   ax.set_xlabel("Squirrel species")
   ax.set_ylabel("F1 score")
 
-png_name = 'rnd classification original.png'
+png_name = 'rnd classification 24000.png'
 save_path = os.path.join(Project_path, 'Figures', png_name)   
 fig.savefig(save_path)
 plt.show()
@@ -68,7 +66,149 @@ msg = """ From: Mrugank Colab <mrugank@gmail.com>
 To: Mrugank Colab <mrugank@gmail.com>
 Subject: SMTP e-mail test
 
-Hi Mrugank, Mrugank here. The original random forest has been trained!"""
+Hi Mrugank, Mrugank here. The 24000 random forest has been trained!"""
+
+server.sendmail("mrugankdake@gmail.com", "mrugankdake@gmail.com", msg)
+server.quit()
+
+
+fig1 = plt.figure(figsize=(18,10))
+
+ax1 = plt.gca()
+for f in feats:
+  # Load data from pickle files
+  path_here = os.path.join(Project_path, 'Data/rnd.pickle')
+  with open(path_here, 'rb') as savef:
+    squirrels = pickle.load(savef)
+  squirrels = np.transpose(np.array(squirrels))
+  audio_feats_data, species, num_vecs = squirrels
+  SQUIRRELS_LIST = []
+  for i in range(audio_feats_data.shape[0]):
+    toto = np.array(audio_feats_data[i], dtype = ('O')).astype(np.float)
+    SQUIRRELS_LIST.append(toto)
+  SQUIRRELS = np.array(SQUIRRELS_LIST)
+  cm, cm_labs, average_acc, accuracies, cm_values = multi_class_classification_a(SQUIRRELS, species, k_fold=k_folds)
+  
+  plot_multi_class_recalls(accuracies, cm_labs, average_acc, cm_values, 'species', f)
+  ax1.set_title('Species classification')
+  ax1.set_xlabel("Squirrel species")
+  ax1.set_ylabel("F1 score")
+
+png_name = 'rnd classification 15000.png'
+save_path = os.path.join(Project_path, 'Figures', png_name)   
+fig1.savefig(save_path)
+plt.show()
+
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("mrugankdake@gmail.com", "MRUGank19@")
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+msg = """ From: Mrugank Colab <mrugank@gmail.com>
+To: Mrugank Colab <mrugank@gmail.com>
+Subject: SMTP e-mail test
+
+Hi Mrugank, Mrugank here. The 15000 random forest has been trained!"""
+
+server.sendmail("mrugankdake@gmail.com", "mrugankdake@gmail.com", msg)
+server.quit()
+
+fig2 = plt.figure(figsize=(18,10))
+
+ax2 = plt.gca()
+for f in feats:
+  # Load data from pickle files
+  path_here = os.path.join(Project_path, 'Data/rnd.pickle')
+  with open(path_here, 'rb') as savef:
+    squirrels = pickle.load(savef)
+  squirrels = np.transpose(np.array(squirrels))
+  audio_feats_data, species, num_vecs = squirrels
+  SQUIRRELS_LIST = []
+  for i in range(audio_feats_data.shape[0]):
+    toto = np.array(audio_feats_data[i], dtype = ('O')).astype(np.float)
+    SQUIRRELS_LIST.append(toto)
+  SQUIRRELS = np.array(SQUIRRELS_LIST)
+  cm, cm_labs, average_acc, accuracies, cm_values = multi_class_classification_b(SQUIRRELS, species, k_fold=k_folds)
+  
+  plot_multi_class_recalls(accuracies, cm_labs, average_acc, cm_values, 'species', f)
+  ax2.set_title('Species classification')
+  ax2.set_xlabel("Squirrel species")
+  ax2.set_ylabel("F1 score")
+
+png_name = 'rnd classification 15000.png'
+save_path = os.path.join(Project_path, 'Figures', png_name)   
+fig2.savefig(save_path)
+plt.show()
+
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("mrugankdake@gmail.com", "MRUGank19@")
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+msg = """ From: Mrugank Colab <mrugank@gmail.com>
+To: Mrugank Colab <mrugank@gmail.com>
+Subject: SMTP e-mail test
+
+Hi Mrugank, Mrugank here. The 7000 random forest has been trained!"""
+
+server.sendmail("mrugankdake@gmail.com", "mrugankdake@gmail.com", msg)
+server.quit()
+
+fig3 = plt.figure(figsize=(18,10))
+
+ax3 = plt.gca()
+for f in feats:
+  # Load data from pickle files
+  path_here = os.path.join(Project_path, 'Data/rnd.pickle')
+  with open(path_here, 'rb') as savef:
+    squirrels = pickle.load(savef)
+  squirrels = np.transpose(np.array(squirrels))
+  audio_feats_data, species, num_vecs = squirrels
+  SQUIRRELS_LIST = []
+  for i in range(audio_feats_data.shape[0]):
+    toto = np.array(audio_feats_data[i], dtype = ('O')).astype(np.float)
+    SQUIRRELS_LIST.append(toto)
+  SQUIRRELS = np.array(SQUIRRELS_LIST)
+  cm, cm_labs, average_acc, accuracies, cm_values = multi_class_classification_c(SQUIRRELS, species, k_fold=k_folds)
+  
+  plot_multi_class_recalls(accuracies, cm_labs, average_acc, cm_values, 'species', f)
+  ax3.set_title('Species classification')
+  ax3.set_xlabel("Squirrel species")
+  ax3.set_ylabel("F1 score")
+
+png_name = 'rnd classification 15000.png'
+save_path = os.path.join(Project_path, 'Figures', png_name)   
+fig3.savefig(save_path)
+plt.show()
+
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("mrugankdake@gmail.com", "MRUGank19@")
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+msg = """ From: Mrugank Colab <mrugank@gmail.com>
+To: Mrugank Colab <mrugank@gmail.com>
+Subject: SMTP e-mail test
+
+Hi Mrugank, Mrugank here. The 3000 random forest has been trained!"""
 
 server.sendmail("mrugankdake@gmail.com", "mrugankdake@gmail.com", msg)
 server.quit()
