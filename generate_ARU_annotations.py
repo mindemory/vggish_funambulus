ebf0315e1ec0123349db4e774fbda4c8bdfbbf44
squirrelcolab <mrugankdevendradake@students.iisertirupati.ac.in>
2020-12-24 07:25:12 +0000

    detector validation fixed FINALLY!

--- generate_ARU_annotations.py
+++ generate_ARU_annotations.py
@@ -17,15 +17,15 @@ from save_text import make_annotation_file, make_day_annotation_file
 from imblearn.under_sampling import RandomUnderSampler
 from analysis_libs_funambulus_with_noise import rf_classifier_aru, rf_classifier_aru_simple  
 
-NOISE = [3000]#, 5000, 10000]        
-TREES = [100]#, 300, 500, 1000]   
-DEPTH = [50]#, 80, 100, 120]      
+NOISE = [3000, 5000, 10000]        
+TREES = [100, 300, 500, 1000]   
+DEPTH = [50, 80, 100, 120]      
 #SAMPLESPLIT = 2#, 5, 10, 15, 20, 50, 100]       
 #max_depth = 50  
 #samplesleaf = 1 
 BOOTSTRAP = [True, False]        
 randomstate = 0 
-classweight = ['balanced']#, 'balanced_subsample']
+classweight = ['balanced', 'balanced_subsample']
 for bootstrap in BOOTSTRAP:
   for classw in classweight:     
     for depth in DEPTH: 
@@ -34,8 +34,8 @@ for bootstrap in BOOTSTRAP:
           print('We are in samples depth = {}, tree_count = {}, noise_value = {}'.format(depth, tree_count, noise_value))     
           days = ['ratufa_whole_day', 'dusky']
           Project_path = '/content/drive/My Drive/Sciurid Lab/CNN/VGGish_Squirrels/'
-          #/input('Project path: ')
           threshold = 0.5
+
           # Load training data from pickle files
           path_here = os.path.join(Project_path, 'Data/rnd19_with_labels.pickle')
           with open(path_here, 'rb') as savef:
@@ -52,8 +52,6 @@ for bootstrap in BOOTSTRAP:
           for day in days:
             folder_name = Project_path + 'ARU_embeddings/' + day + '/'
             file_names = sorted(os.listdir(folder_name))
-            #print(file_names[0:3])
-            #print(['We are on day ' + day])
 
             save_folder = Project_path+'Detections19/'+str(noise_value)+'_noise_'+str(tree_count)+'trees_'+str(depth)+'depth_classweight_'+classw+'_bootstrap_' + str(bootstrap) + '_classifier/' 
             if not os.path.exists(save_folder):     
@@ -80,7 +78,6 @@ for bootstrap in BOOTSTRAP:
               species_prediction.append(predictions)
               species_prediction = np.transpose(np.asarray(species_prediction))
               species_prediction_day.append(np.asarray(species_prediction))
-              #species_prediction[species_prediction == 'AAA'] = 'NOISE'
               num_preds += species_prediction.shape[0]
               num_preds_file.append(num_preds)
 
@@ -94,7 +91,6 @@ for bootstrap in BOOTSTRAP:
               duration_files.append(duration)	
             
             species_prediction_day = np.asarray(species_prediction_day)	
-            #print(species_prediction_day.shape)
             save_path_day = save_folder + day + '.txt'	
             make_day_annotation_file(save_path_day, species_prediction_day, num_preds_file, duration_files)
 print(time.time() - start_time)
